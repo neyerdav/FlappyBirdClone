5585d70d1490cc433d588786c3ecb248cbe45400
David Neyer <neyer.david@googlemail.com>
2020-10-01 10:55:21 +0200

    Added Space key Listener

--- src/game/Input.java
+++ src/game/Input.java
@@ -1,4 +1,38 @@
 package game;
 
-public class Input {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Input implements KeyListener {
+
+    private boolean spacePressed = false;
+    private boolean spaceReleased = true;
+
+    public boolean isSpacePressed() {
+        boolean s = spacePressed;
+        spacePressed = false;
+        return s;
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE && spaceReleased) {
+            spacePressed = true;
+            spaceReleased = false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            spaceReleased = true;
+        }
+
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
 }
